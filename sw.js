723e3094496ceea9829237b19f207214a3cebb6d
qiessertbites <78545779+qiessertbites@users.noreply.github.com>
2026-07-20 17:27:15 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,29 @@
-const CACHE_NAME = "qiessert-bites-v1";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json",
-  "./images/cakechocolate.webp",
-  "./images/coffee.jpg",
-  "./images/pasta.webp"
-];
+const CACHE_NAME = "qiessert-bites-v999";
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        fetch(event.request, {
+            cache: "no-store"
+        }).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
